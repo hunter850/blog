8dee0811e00264bcdd69760ea25c206e1e95bc43
hunter850 <hunter850711@gmail.com>
2025-01-27 14:38:59 +0800

    [Update] add text to test auto deploy

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,7 @@ export default function Home() {
             <main className="row-start-2 flex flex-col items-center gap-8 sm:items-start">
                 <Image className="dark:invert" src="/next.svg" alt="Next.js logo" width={180} height={38} priority />
                 <ol className="list-inside list-decimal text-center font-[family-name:var(--font-geist-mono)] text-sm sm:text-left">
+                    <li className="mb-2">test</li>
                     <li className="mb-2">
                         Get started by editing{" "}
                         <code className="rounded bg-black/[.05] px-1 py-0.5 font-semibold dark:bg-white/[.06]">
