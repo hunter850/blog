057027bf17b7027b6f997aeda666e56111c401e6
hunter850 <hunter850711@gmail.com>
2025-02-08 02:12:15 +0800

    [Fix] fix ms_login/page.tsx doesn't have a root layout.

--- src/app/ms_login/page.tsx
+++ src/app/ms_login/page.tsx
@@ -1,9 +1,16 @@
 import MsLoginPage from "@/components/pages/MsLoginPage";
+import { Roboto } from "next/font/google";
+const roboto = Roboto({
+    weight: ["300", "400", "500", "700"],
+    subsets: ["latin"],
+    display: "swap",
+    variable: "--font-roboto",
+});
 
 function MsLogin(): React.JSX.Element {
     return (
         <html>
-            <body>
+            <body className={`${roboto.variable} antialiased`} id="root">
                 <MsLoginPage />
             </body>
         </html>
