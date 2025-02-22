fe24767e53d5e8d9f308f979e46fda8b8ee8e904
hunter850 <hunter850711@gmail.com>
2025-02-23 01:57:09 +0800

    [Feat] add front page

--- src/components/pages/HomePage/HomePage.tsx
+++ src/components/pages/HomePage/HomePage.tsx
@@ -1,15 +1,21 @@
 import Header from "@/components/organisms/Header";
-import FeaturedPost from "@/components/organisms/FeaturedPost";
-import RecentPosts from "@/components/organisms/RecentPosts";
 import Footer from "@/components/organisms/Footer";
 
 export default function Home() {
     return (
         <div className="flex min-h-screen flex-col">
             <Header />
-            <main className="container mx-auto mt-16 flex-grow px-4 py-8">
-                <FeaturedPost />
-                <RecentPosts />
+            <main className="container mx-auto flex flex-grow flex-col items-center justify-center space-y-6 px-4 py-8 text-center">
+                <h2 className="bg-gradient-to-r from-blue-500 to-cyan-500 bg-clip-text text-5xl text-transparent [-webkit-background-clip:text] dark:from-blue-400 dark:to-cyan-400 md:text-7xl lg:text-8xl">
+                    Kevin
+                </h2>
+
+                <h3 className="text-center text-2xl text-gray-700 dark:bg-gradient-to-r dark:from-blue-400 dark:to-cyan-400 dark:bg-clip-text dark:text-transparent dark:[-webkit-background-clip:text] md:text-3xl lg:text-5xl">
+                    Professional Frontend Engineer
+                </h3>
+                <p className="text-center text-base leading-relaxed text-gray-600 dark:text-gray-400 md:text-xl">
+                    Dedicated to building modern and useful web applications.
+                </p>
             </main>
             <Footer />
         </div>
