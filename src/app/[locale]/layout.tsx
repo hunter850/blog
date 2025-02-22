5e35252ab2c77f409f2b59abb1cf0c3878fa3824
hunter850 <hunter850711@gmail.com>
2025-02-22 23:01:35 +0800

    [Feat] add auth group route

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,7 +1,32 @@
+import { Roboto } from "next/font/google";
+import { notFound } from "next/navigation";
 import { NextIntlClientProvider } from "next-intl";
 import { getMessages } from "next-intl/server";
-import { notFound } from "next/navigation";
+import ThemeProvider from "@/providers/ThemeProvider";
 import { routing } from "@/i18n/routing";
+import "@/app/globals.css";
+// types
+import type { Metadata } from "next";
+
+const roboto = Roboto({
+    weight: ["300", "400", "500", "700"],
+    subsets: ["latin"],
+    display: "swap",
+    variable: "--font-roboto",
+});
+
+export const metadata: Metadata = {
+    title: "HLKW | Blog",
+    description: "HLKW's blog",
+    icons: { icon: { url: "/images/favicon.ico" } },
+    authors: [{ name: "HLKW" }],
+    openGraph: {
+        title: "HLKW | Blog",
+        description: "Hi, I'm HLKW, a Frontend Developer.",
+        locale: "zh_TW",
+        type: "website",
+    },
+};
 
 export default async function LocaleLayout({
     children,
@@ -20,5 +45,13 @@ export default async function LocaleLayout({
     // side is the easiest way to get started
     const messages = await getMessages();
 
-    return <NextIntlClientProvider messages={messages}>{children}</NextIntlClientProvider>;
+    return (
+        <html suppressHydrationWarning lang={locale}>
+            <body className={`${roboto.variable} antialiased`} id="root">
+                <ThemeProvider>
+                    <NextIntlClientProvider messages={messages}>{children}</NextIntlClientProvider>;
+                </ThemeProvider>
+            </body>
+        </html>
+    );
 }
--- src/app/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function RootPage() {
-    return (
-        <html>
-            <body></body>
-        </html>
-    );
-}
