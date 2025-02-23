6cf37071701d112c4a02be295ab4a6a6f88a2509
hunter850 <hunter850711@gmail.com>
2025-02-23 15:07:05 +0800

    [Style] adjust background color of footer in dark mode

--- src/components/organisms/Footer/Footer.tsx
+++ src/components/organisms/Footer/Footer.tsx
@@ -1,8 +1,8 @@
 function Footer() {
     return (
         <footer className="bg-muted py-4">
-            <div className="text-muted-foreground container mx-auto text-center">
-                <p className="text-center">&copy; 2025 HLKW.me. All rights reserved.</p>
+            <div className="container mx-auto text-center text-muted-foreground">
+                <p className="text-center">&copy; 2025 HLKW.me All rights reserved.</p>
             </div>
         </footer>
     );
