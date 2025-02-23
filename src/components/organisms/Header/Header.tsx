cab0b858a90d82ca8775d1560a5bc60f4628559a
hunter850 <hunter850711@gmail.com>
2025-02-23 22:55:50 +0800

    [Style] constrain the max width of the header

--- src/components/organisms/Header/Header.tsx
+++ src/components/organisms/Header/Header.tsx
@@ -13,7 +13,7 @@ function Header() {
     const t = useTranslations();
     return (
         <header className="sticky left-0 right-0 top-0 z-50 border-0 border-b border-solid border-gray-300 bg-background">
-            <div className="container mx-auto px-4 sm:px-6 lg:px-8">
+            <div className="container mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
                 <div className="flex h-16 items-center justify-between">
                     <Logo />
                     <div className="hidden items-center space-x-4 sm:flex">
