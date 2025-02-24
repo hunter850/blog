710a0f9d84dba80dca488fc00279548bc8192a5d
hunter850 <hunter850711@gmail.com>
2025-02-24 12:59:37 +0800

    [Fix] fix title link redirection do not based on language

--- src/components/atoms/Logo/Logon.tsx
+++ src/components/atoms/Logo/Logon.tsx
@@ -1,8 +1,10 @@
 import Link from "next/link";
+import { useLocale } from "next-intl";
 
 function Logo() {
+    const locale = useLocale();
     return (
-        <Link href="/" className="text-2xl font-bold">
+        <Link href={`/${locale}`} className="text-2xl font-bold">
             HLKW.me
         </Link>
     );
