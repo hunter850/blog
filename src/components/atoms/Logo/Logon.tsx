bcec4443f563579fab382d1229c60feabddfa61e
hunter850 <hunter850711@gmail.com>
2025-02-24 16:04:11 +0800

    [Feat] add icon to header

--- src/components/atoms/Logo/Logon.tsx
+++ src/components/atoms/Logo/Logon.tsx
@@ -1,11 +1,15 @@
 import Link from "next/link";
+import Image from "next/image";
 import { useLocale } from "next-intl";
 
 function Logo() {
     const locale = useLocale();
     return (
-        <Link href={`/${locale}`} className="text-2xl font-bold">
-            HLKW.me
+        <Link href={`/${locale}`}>
+            <div className="flex cursor-pointer items-center gap-2">
+                <Image src="/images/favicon-96x96.png" alt="HoloBoard Logo" width={36} height={36} />
+                <h1 className="text-2xl font-bold">HLKW.me</h1>
+            </div>
         </Link>
     );
 }
