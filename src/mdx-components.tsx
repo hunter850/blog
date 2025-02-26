23ada0de9a0d32deccdd94ecfa121fc3cc931a90
hunter850 <hunter850711@gmail.com>
2025-02-26 16:13:41 +0800

    [Style] remove all styles from mdx-components

--- src/mdx-components.tsx
+++ src/mdx-components.tsx
@@ -3,8 +3,6 @@ import "./app/globals.css";
 
 export function useMDXComponents(components: MDXComponents): MDXComponents {
     return {
-        h2: ({ children }) => <h2 className="bg-red-500 text-2xl font-bold">{children}</h2>,
-
         ...components,
     };
 }
