568700c54dd743426bf59eca80d48519b530f30f
hunter850 <hunter850711@gmail.com>
2025-03-03 22:26:40 +0800

    [Chore] update image settings for optimized loading

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,11 @@ const withMDX = createMDX({});
 const nextConfig: NextConfig = {
     transpilePackages: ["next-mdx-remote"],
     pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
+    images: {
+        deviceSizes: [640, 1200],
+        imageSizes: [],
+        formats: ["image/webp"],
+    },
 };
 
 export default withMDX(nextConfig);
